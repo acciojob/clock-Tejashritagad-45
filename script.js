8efd91cc06a46e60c1af715954615277313d8f9f
Tejashri Tagad <tagadteju45@gmail.com>
2025-12-16 21:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+setInterval(()=>{
+	const timerEle = document.querySelector("#timer");
+	let now = new Date();
+	let dateTime = now.toLocaleString();
+	timerEle.textContent = dateTime;
+},1000);
